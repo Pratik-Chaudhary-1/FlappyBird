4c6b6ff8ac82270d3a98c2011515ecb768e89f9c
Pratik Chaudhary <pratikchaudhary685@gmail.com>
2025-11-20 14:03:03 -0500

    added basic game configuration and changed js files to jsx

---
+++ game/src/Config.jsx
@@ -0,0 +1,11 @@
+export const GAME_CONFIG = {
+  width: 800,
+  height: 600,
+  playerSize: 40,
+  gravity: 0.6,
+  jumpStrength: -12,
+  moveSpeed: 8,
+  obstacleWidth: 60,
+  obstacleSpeed: 3,
+  gapSize: 100,
+};
