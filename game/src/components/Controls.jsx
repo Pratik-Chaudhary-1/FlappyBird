99a55eca5118de5c4a305e7fee203e90e874d536
Pratik Chaudhary <pratikchaudhary685@gmail.com>
2025-11-20 15:16:13 -0500

    added controls and gamespace components

---
+++ game/src/components/Controls.jsx
@@ -0,0 +1,10 @@
+const Controls = () => {
+  return (
+    <div className="controls">
+      <p className="controls-title">Controls:</p>
+      <P>SPACE/↑ - Jump | ← → - Move Left/Right | R - Restart</P>
+    </div>
+  );
+};
+
+export default Controls;
