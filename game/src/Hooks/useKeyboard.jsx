880d7615066046f6293ccd62c8dd4a9b5fdba2be
Pratik Chaudhary <pratikchaudhary685@gmail.com>
2025-11-20 15:31:27 -0500

    added eventlistner logic for keys pressed

---
+++ game/src/Hooks/useKeyboard.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useRef } from "react";
+
+export const useKeyboard = (gameOver, gameStarted, onJump, onRestart) => {
+  const keysPressed = useRef({});
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.code === "Space" && !gameStarted) {
+        onJump();
+        return;
+      }
+
+      if (e.code === "Space" || e.code === "ArrowUp") {
+        e.preventDefault();
+        if (!gameOver && gameStarted) {
+          onJump();
+        }
+      }
+
+      if (e.code === "ArrowLeft" || e.code === "ArrowRight") {
+        e.preventDefault();
+        keysPressed.current[e.code] = true;
+      }
+
+      if (e.code === "keyR" && gameOver) {
+        onRestart();
+      }
+    };
+
+    const handleKeyUp = (e) => {
+      if (e.code === "ArrowLeft" || e.code === "ArrowRight") {
+        keysPressed.current[e.code] = false;
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  }, [gameOver, gameStarted, onJump, onRestart]);
+
+  return keysPressed;
+};
